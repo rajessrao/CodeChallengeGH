d43dc96b813d62f3b0898b08c0d3261b843d12fb
Rajesh Botchu <rajessrao@gmail.com>
2022-03-03 16:36:34 +0530

    Code cleaning and commenting

--- index.js
+++ index.js
@@ -1,18 +1,15 @@
 const minimist = require('minimist')
 const chalk = require('chalk')
-const config = require('./src/config')
 let comments = require('./src/controllers/commentsController')
 
-// console.log(chalk.yellow('Your github token is:'))
-// console.info(chalk.yellow(config.GITHUB_PERSONAL_ACCESS_TOKEN))
-
 let argv = minimist(process.argv)
+
+// Getting arguments (repo & period)
 const repo = argv.repo
 const period = argv.period
-// console.log(repo, period)
 
+// Validating arguments (repo & period)
 if (typeof repo === 'string' && repo.match(/(\w)|(\/)|(-)/g)) {
-  // console.log('Valid repo.')
   let days = 0
   if (typeof period === 'string' && period.includes('d')) {
     days = period.replace(/([d])/g, '')
@@ -25,12 +22,14 @@ if (typeof repo === 'string' && repo.match(/(\w)|(\/)|(-)/g)) {
     date.setDate(date.getDate() - time)
     dateISOString = date.toISOString().replace(/(\..*)/g, 'Z')
   }
-  // console.log(dateISOString)
+
+  // Log info
+  console.log('\n')
   console.log(`Fetching comments for past ${days} days for ${repo}`)
+  console.log('\n')
+
+  // Start getting comments data
   comments(repo, dateISOString)
 } else {
-  console.log('Invalid repo, try again with valid repo.')
+  console.log(chalk.red('Invalid repo, try again with valid repo.'))
 }
-
-// remove this line
-// require('./example')
--- src/controllers/commentsController.js
+++ src/controllers/commentsController.js
@@ -3,32 +3,41 @@ const refineData = require('../helpers/refineData')
 const reportData = require('../helpers/reportData')
 
 module.exports = async function (repo, dateISOString) {
+  // Fetching comments data with comments, issues, pulls and stats
   let comments = await githubService.getGitData(repo, dateISOString, 'comments')
   let issues = await githubService.getGitData(repo, dateISOString, 'issues')
   let pulls = await githubService.getGitData(repo, dateISOString, 'pulls')
   let stats = await githubService.getGitStatsData(repo)
+
+  // Putting all data together
   let totalData = []
 
+  // Adding comments data to totalData
   if (comments.length) {
     totalData.push(comments)
   }
 
+  // Adding issues data to totalData
   if (issues.length) {
     totalData.push(issues)
   }
 
+  // Adding pulls data to totalData
   if (pulls.length) {
     totalData.push(pulls)
   }
 
+  // Adding stats data to totalData
   if (stats.length) {
     totalData.push(stats)
   }
 
-  // console.log('totalData ::::::::::::::::::::: \n', totalData)
-
+  // Refining the totalData i.e combining and sorting
   let refinedData = await refineData(totalData)
 
-  // Data report
+  /**
+   * Reporting data in the below format
+   * 3012 comments, michael.davidovich (20 commits)
+   */
   reportData(refinedData)
 }

ad00d853affa8a811ed898822b5302834ae628ec
Rajesh Botchu <rajessrao@gmail.com>
2022-03-03 17:55:41 +0530

    No data condition

--- src/helpers/reportData.js
+++ src/helpers/reportData.js
@@ -2,16 +2,20 @@ const leftPad = require('left-pad')
 
 const reportData = {
   getReport(users) {
-    const spaces = users[0].comments.toString().length
+    if (users[0] && users[0].comments) {
+      const spaces = users[0].comments.toString().length
 
-    users.map((user) => {
-      let comments = leftPad(user.comments.toString(), spaces)
-      let login = user.login
-      let commits = user.total
+      users.map((user) => {
+        let comments = leftPad(user.comments.toString(), spaces)
+        let login = user.login
+        let commits = user.total
 
-      // Logging report data
-      console.log(`${comments} comments, ${login} (${commits} commits)`)
-    })
+        // Logging report data
+        console.log(`${comments} comments, ${login} (${commits} commits)`)
+      })
+    } else {
+      console.log('No comments available.')
+    }
   },
 }
 
